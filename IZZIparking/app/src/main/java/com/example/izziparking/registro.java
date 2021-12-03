ecaae2500df45c883bc5e0874625f56b49299634
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2021-12-03 00:02:41 -0500

    creacion de interfaces
    se Crean interfaces basicas

--- IZZIparking/app/src/main/java/com/example/izziparking/registro.java
+++ IZZIparking/app/src/main/java/com/example/izziparking/registro.java
@@ -7,19 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button boton_registro;
+public class registro extends AppCompatActivity {
+    Button registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        boton_registro = (Button)findViewById(R.id.boton_registro);
-        boton_registro.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_registro);
+
+
+
+        registro = (Button)findViewById(R.id.boton_registro);
+        registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(MainActivity.this, registro.class);
+                Intent i = new Intent(registro.this, MainActivity.class);
                 startActivity(i);
 
             }
