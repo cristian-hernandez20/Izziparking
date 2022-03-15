9385a08da8ba92f82f1ea87e6d0ca89179f97633
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2022-03-15 06:23:02 -0500

    Creacion proyecto
    Se crea proyecto en git

---
+++ IZZparking/app/src/main/java/com/example/proyectoizziparking/proyecto_izziparking.java
@@ -0,0 +1,14 @@
+package com.example.proyectoizziparking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class proyecto_izziparking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyecto_izziparking);
+    }
+}
